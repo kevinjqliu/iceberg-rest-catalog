4d6563a783dc555f4743b61c315432332b4e74cb
Kevin Liu <kevinjqliu@users.noreply.github.com>
2024-06-04 18:14:51 -0700

    remove unused code

--- main.py
+++ main.py
@@ -12,28 +12,6 @@ from pyiceberg.exceptions import TableAlreadyExistsError, NoSuchTableError, Name
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-
 from pyiceberg.catalog.sql import SqlCatalog
 
 warehouse_path = "/tmp/warehouse"
