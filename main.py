be3e243d08399bdd2bcb95fabde066dfa2463092
Kevin Liu <kevinjqliu@users.noreply.github.com>
2024-06-10 11:03:12 -0700

    add fastapi main method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from api.catalog_api import router as CatalogApiRouter
 from catalog import get_catalog
 from exception import IcebergHTTPException, iceberg_http_exception_handler
@@ -13,3 +14,7 @@ app.add_exception_handler(IcebergHTTPException, iceberg_http_exception_handler)
 for catalog in get_catalog():
     catalog.destroy_tables()
     catalog.create_tables()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
