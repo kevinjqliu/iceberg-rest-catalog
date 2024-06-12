d4cd00f0edfd24f1cb961ac9747436e7a444d166
Kevin Liu <kevinjqliu@users.noreply.github.com>
2024-06-12 09:38:52 -0700

    wrap main create app function

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from api.catalog_api import router as CatalogApiRouter
 from exception import IcebergHTTPException, iceberg_http_exception_handler
 
 
-app = FastAPI()
-app.include_router(CatalogApiRouter)
-app.add_exception_handler(IcebergHTTPException, iceberg_http_exception_handler)
+def create_app():
+    app = FastAPI()
+    app.include_router(CatalogApiRouter)
+    app.add_exception_handler(IcebergHTTPException, iceberg_http_exception_handler)
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
